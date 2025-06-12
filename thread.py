9c477d13e405592a17b866f691928fa0e0d333cb
mohamed elkhamaty <mohamedelkhamaty6@gmail.com>
2025-06-12 23:02:02 +0300

    now i need to be sure about streamlit

---
+++ thread.py
@@ -0,0 +1,23 @@
+import streamlit as s
+import os
+s.set_page_config(page_icon='C:/Users/elkha/Downloads/atom_molecule_science_school_knowledge_chemistry_learning_education_icon_266619.ico',page_title='hello',layout="centered")
+s.title('33 ')
+
+s.write(False)
+print("r")
+s.write({'jjjjj':'hg' 'jdv' 'ee' })
+p=s.button('prjess')
+print(p)
+s1=s.chat_input()
+s2=s.camera_input('enter')
+if(s2):
+    with open(f"{s2.name}.jpg",'wb') as f:
+        f.write(s2.getvalue())
+if(s1):
+    print(s1)
+e='''
+def e(nm):
+    print('hellodfsffsfsfsfsfssfsfhsd sf sf s s   sg s gs gs g ',name )'''
+s.code(e,wrap_lines=True,language='python')
+def e(nm):
+    print('hellodfsffsfsfsfsfssfsfhsd sf sf s s   sg s gs gs g ',name )
