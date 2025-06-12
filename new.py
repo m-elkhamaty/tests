a1112fa8efb6a3884e1aa90eb1a5cf3081999b35
mohamed elkhamaty <mohamedelkhamaty6@gmail.com>
2025-06-12 23:11:58 +0300

    j

--- new.py
+++ new.py
@@ -1,1 +1,20 @@
-print(5)
+class EvenNumbers:
+    def __iter__(self):
+        self.n = 4  # Start from the first even number
+        return self
+
+    def __next__(self):
+        x = self.n
+        print(454) # Increment by 2 to get the next even number
+        return x
+
+# Create an instance of EvenNumbers
+even = EvenNumbers()
+it = iter(EvenNumbers())
+
+# Print the first five even numbers
+print(next(it))  
+print(next(it)) 
+print(next(it))  
+print(next(it)) 
+print(next(it))
