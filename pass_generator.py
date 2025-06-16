6b3e6e011a867ced87934b2bceac7f9a8011959e
mohamed elkhamaty <mohamedelkhamaty6@gmail.com>
2025-06-16 11:09:14 +0300

    pass_generator

---
+++ pass_generator.py
@@ -0,0 +1,49 @@
+import random
+import string
+
+
+
+def pass_generator(min_chars,max_chars,digit=True,special=True):
+    letters=string.ascii_letters
+    digits=string.digits
+    specials=string.punctuation
+    
+    characters=letters
+    if digit:
+        characters+=digits
+    if special:
+        characters+=specials
+    done=False
+    hasd=False
+    hass=False
+
+    psw=''
+    try:
+        length=random.randint(min_chars,max_chars)
+
+        while not done or len(psw)<length  :
+            psw+=characters[random.randint(0,len(characters)-1)]
+            if digit and special:
+                for i in psw:
+                    if i in digits:
+                        hasd=True
+                    if i in specials:
+                        hass=True
+                if hasd and hass:
+                    done=True
+            elif digit :
+                for i in psw:
+                    if i in digits:
+                        hasd=True
+                if hasd :
+                    done=True
+            else:
+                done=True
+            
+        return psw
+    except Exception as e:
+        print('max must be more than min')
+        return
+
+
+print(pass_generator(9,19,True,True))
